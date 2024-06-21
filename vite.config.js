b2ccec79da580b6dc7f2eb97f3f02377b3f04d25
Neelima <neelimaakkiraju2001@gmail.com>
2024-06-21 19:03:05 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      // Configure external modules if needed
+      external: ['/src/main.jsx'],
+    },
+  },
+  extensions: ['.js', '.jsx'],
   plugins: [react()],
   base: '/react-dashboard/', 
 });
